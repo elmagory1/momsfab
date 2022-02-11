33320f58d702865798a775bf6605c080473eab9c
jiloysss <janlloydangeles@gmail.com>
2022-02-11 11:45:56 +0800

    Update

--- momsfab/doc_events/purchase_order.py
+++ momsfab/doc_events/purchase_order.py
@@ -1,29 +1,7 @@
 import frappe
 from frappe.model.mapper import get_mapped_doc
 
-@frappe.whitelist()
-def generate_gate_pass(source_name, target_doc=None):
-    print("==================================================")
-    doc = get_mapped_doc("Purchase Order", source_name, {
-        "Purchase Order": {
-            "doctype": "Gate Pass",
-            "validation": {
-                "docstatus": ["=", 1]
-            }
-        },
-        "Purchase Order Item": {
-            "doctype": "Gate Pass Items",
-        }
 
-    }, target_doc)
-
-    return doc
-
-@frappe.whitelist()
-def check_gate_pass(name):
-    gate_pass = frappe.db.sql(""" SELECT COUNT(*) as count FROM `tabGate Pass` WHERE purchase_order=%s and docstatus < 2 """, name,as_dict=1)
-
-    return gate_pass[0].count > 0
 
 @frappe.whitelist()
 def on_submit_po(doc, method):
--- momsfab/public/js/purchase_order.js
+++ momsfab/public/js/purchase_order.js
@@ -1,25 +1,8 @@
 
 frappe.ui.form.on('Purchase Order', {
-	refresh: function () {
-        frappe.call({
-            method: "momsfab.doc_events.purchase_order.check_gate_pass",
-            args: {
-                name: cur_frm.doc.name
-            },
-            callback: function (r) {
-                if(!r.message && cur_frm.doc.docstatus){
-                     cur_frm.add_custom_button(__("Gate Pass"), () => {
-                         frappe.model.open_mapped_doc({
-                            method: "load_controls.doc_events.purchase_order.generate_gate_pass",
-                            frm: cur_frm
-                        })
-
-                    }, 'Create');
-                }
-            }
-        })
-
-
-    }
+    // refresh: function () {
+    //
+    //
+    // }
 
 });
